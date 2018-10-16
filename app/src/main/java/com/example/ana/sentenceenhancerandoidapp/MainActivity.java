5a16ed2609d2cf0236b0c22f4ac258c94021f347
Theja Krishna <takrishn@uci.edu>
2018-10-15 17:09:39 -0700

    added a simple interface with working button
    button reproduces whatever is in the text field

--- app/src/main/java/com/example/ana/sentenceenhancerandoidapp/MainActivity.java
+++ app/src/main/java/com/example/ana/sentenceenhancerandoidapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.ana.sentenceenhancerandoidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        final EditText txt = findViewById(R.id.editText);
+        final TextView txtview = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+                Log.i("tag", txt.getText().toString());
+                txtview.setText(txt.getText().toString());
+
+            }
+        });
     }
 }
